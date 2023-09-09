c297d675545ff0e4ad4c0d8c054dbcee91844129
anirbanpranto <anirbanpranto@gmail.com>
2023-09-10 00:16:40 +0800

    fix: added Pipeline type for sklearn

--- inference/utils.py
+++ inference/utils.py
@@ -1,8 +1,9 @@
 import joblib
-from typing import Dict, Any
+from typing import Dict
+from sklearn.pipeline import Pipeline
 
 
-def load_model_from_artifact(artifact_name: str) -> Any:
+def load_model_from_artifact(artifact_name: str) -> Pipeline:
     # ideally this should be fetched from some kind of storage -> (i.e. - S3)
     return joblib.load("inference/artifacts/" + artifact_name)
 
@@ -19,11 +20,11 @@ def get_column_map() -> Dict:
 
 
 class InferenceModel:
-    model = None
+    model: Pipeline = None
 
     # to make sure we don't load the model for every request
     @staticmethod
-    def get_model() -> Any:
+    def get_model() -> Pipeline:
         if InferenceModel.model:
             return InferenceModel.model
         else:
